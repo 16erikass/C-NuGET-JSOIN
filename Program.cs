80ec814089d5249549b1da1b506d5c7dc7f1c8a7
16erikass <16erikass@gmail.com>
2026-04-13 18:37:13 +0300

    Papildymas

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿using Newtonsoft.Json;
-
+﻿using Newtonsoft.Json; // Biblioteka, skirta JSON konvertavimui.
+//Vartotojo aprašas.
 class User
 {
     public string Name { get; set; } = "";
     public int Age { get; set; }
     public string City { get; set; } = "";
 }
-
+//Admin perima visas User savybes ir prideda savo.
 class Admin : User
 {
     public string Permission { get; set; } = "";
@@ -15,14 +15,16 @@ class Admin : User
 class Program
 {
     static void Main(string[] args)
-    {
+    {// Failų pavadinimai, kuriuose bus saugomi duomenis.
         string file1 = "users.json";
         string file2 = "usertypes.json";
 
 
-        string jsonFromFile = File.ReadAllText(file1);
+        string jsonFromFile = File.ReadAllText(file1); //Programa paima tekstą iš failo.
 
-        List<User> users = JsonConvert.DeserializeObject<List<User>>(jsonFromFile) ?? new List<User>();
+        //json pavertimas.
+        //Naudojamas ??: jei failas tuščias, sukuriamas naujas sąrašas.
+        List <User> users = JsonConvert.DeserializeObject<List<User>>(jsonFromFile) ?? new List<User>(); 
 
         Console.WriteLine("USERS:");
         foreach (var u in users)
@@ -32,17 +34,17 @@ class Program
 
         Console.WriteLine();
 
-
+        //Duomenu papildymas.
         users.Add(new User
         {
             Name = "Tom",
             Age = 30,
             City = "Berlin"
         });
-
+        //Sąrašas paverčiamas atgal į tekstą.
         File.WriteAllText(file1, JsonConvert.SerializeObject(users, Formatting.Indented));
 
-
+        //Sukuriamas administratoriaus sąrašas.
         List<Admin> admins = new List<Admin>
         {
             new Admin
@@ -54,9 +56,9 @@ class Program
             }
         };
 
-
+        //išsaugojamas š atskira failą.
         File.WriteAllText(file2, JsonConvert.SerializeObject(admins, Formatting.Indented));
-
+        //nuskaitomas failas.
         List<Admin> loadedAdmins = JsonConvert.DeserializeObject<List<Admin>>(File.ReadAllText(file2)) ?? new List<Admin>();
 
         Console.WriteLine("ADMINS:");
